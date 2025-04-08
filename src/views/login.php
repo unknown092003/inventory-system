3ce5de5b51d8ac6e7ee48718c8be8df58fddedad
errol john <unknown092003@gmail.com>
2025-04-08 09:15:05 +0800

    inital commit

---
+++ src/views/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/inventory-system/public/styles/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="login_form">
+        <img src= "" alt="">
+        <h1>Log In</h1>
+        <input type="username"placeholder="Enter your Username">
+        <input type="password" placeholder="Enter your password">
+        <button type="submit">Log In</button>
+    </div>
+    
+</body>
+</html>
