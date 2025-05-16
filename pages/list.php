82a1c808fb0415216807ea412db44ab5d3de4759
Jay-R-12 <rjaybassit123@gmail.com>
2025-05-16 10:19:55 +0800

    changes

--- pages/list.php
+++ pages/list.php
@@ -58,7 +58,7 @@
         // $im->annotateImage($draw, 600, 288, 0, $row["serial_number"]);
         $im->annotateImage($draw, 590, 350, 0, $row["acquisition_date"]);
         $im->annotateImage($draw, 600, 410, 0, $row["person_accountable"]);
-        $im->annotateImage($draw, 600, 475, 0, $row["status"]);
+        $im->annotateImage($draw, 600, 475, 0, $row["remarks"]);
         $im->annotateImage($draw, 600, 535, 0, $row["signature_of_inventory_team_date"]);
     
         // Save the output
